b7bb169c871f596f90b61846e0d6402e2dd96f7d
gtalpo <gtalpo@gmail.com>
2025-11-01 12:04:17 +0900

    start swayidle & modify pacman-update click

---
+++ config/waybar/scripts/waybar-restart-ex.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# update packages
+yay -Syu
+
+# Kill any running Waybar processes
+pkill -x waybar
+
+# Wait briefly to ensure Waybar is fully terminated
+sleep 1
+
+# Start Waybar again
+nohup waybar >/dev/null 2>&1 &
